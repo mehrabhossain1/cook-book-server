bc5602efc3393268762d65b7a62fd6db02158c0a
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-19 16:04:26 +0600

    all data created

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.json({ message: "hello server running" });
 });
 
+app.get("/allData", (req, res) => {
+  res.send({ result: "okkk" });
+});
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
