5dd6d7a87c69640ea8dc51749714028808e01d93
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-19 16:30:09 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
+const allRecipes = require("./data.json");
 
 app.get("/", (req, res) => {
   res.json({ message: "hello server running" });
 });
 
 app.get("/allData", (req, res) => {
-  res.send({ result: "okkk" });
+  res.send({ allRecipes });
 });
 
 app.listen(5000, () => {
