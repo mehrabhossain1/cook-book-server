1e04fc3300da562eee9249743414304752de8679
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-19 16:00:27 +0600

    nodemon index in pk.json file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ message: "hello server running" });
+});
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
